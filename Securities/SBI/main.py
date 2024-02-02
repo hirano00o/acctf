bb8675b1d8410af7fa1f542a38876580ed6d3ee2
hirano00o <uday6827@gmail.com>
2024-02-03 00:48:11 +0900

    Introduce the scrape of fund to the function
    * Change typing.List to builtin list

--- Securities/SBI/main.py
+++ Securities/SBI/main.py
@@ -1,5 +1,5 @@
+from abc import ABC
 from io import StringIO
-from typing import List
 
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
@@ -8,21 +8,23 @@ from Securities.Base.main import BaseSecurities, Value
 import pandas as pd
 
 
-def _get_formatted(df: pd.DataFrame) -> List[Value]:
+def _get_formatted(df: pd.DataFrame) -> list[Value]:
+    if df is None:
+        return []
     df = df.drop(df.index[0]).iloc[:,0:3]
     code_df = df[::2].iloc[:,[1]].reset_index(drop=True).set_axis(['name'], axis=1)
     val_df = df[1::2].reset_index(drop=True).set_axis(['amount', 'acquisition_val', 'current_val'], axis=1)
-    ret: List[Value] = []
+    ret: list[Value] = []
     for _, row in pd.concat([code_df, val_df], axis=1).iterrows():
         ret.append(Value(row['name'], row['amount'], row['acquisition_val'], row['current_val']))
 
     return ret
 
 
-class SBI(BaseSecurities):
-    _df_fund_specific: pd.DataFrame
-    _df_fund_nisa_accum: pd.DataFrame
-    _df_fund_old_nisa_accum: pd.DataFrame
+class SBI(BaseSecurities, ABC):
+    _df_fund_specific: pd.DataFrame = None
+    _df_fund_nisa_accum: pd.DataFrame = None
+    _df_fund_old_nisa_accum: pd.DataFrame = None
 
     def __init__(self):
         super().__init__()
@@ -39,7 +41,7 @@ class SBI(BaseSecurities):
         return self
 
 
-    def get_stock_specific(self) -> List[Value]:
+    def get_stock_specific(self) -> list[Value]:
         # 口座管理ページ
         self.driver.find_element(By.XPATH, '//*[@id="link02M"]/ul/li[3]/a/img').click()
         # 株式(現物)タブ
@@ -53,7 +55,25 @@ class SBI(BaseSecurities):
         return _get_formatted(df)
 
 
-    def get_fund_specific(self) -> List[Value]:
+    def get_fund_specific(self) -> list[Value]:
+        if self._df_fund_specific is None:
+            self._get_fund_all()
+        return _get_formatted(self._df_fund_specific)
+
+
+    def get_fund_nisa_accum(self) -> list[Value]:
+        if self._df_fund_nisa_accum is None:
+            self._get_fund_all()
+        return _get_formatted(self._df_fund_nisa_accum)
+
+
+    def get_fund_old_nisa_accum(self) -> list[Value]:
+        if self._df_fund_old_nisa_accum is None:
+            self._get_fund_all()
+        return _get_formatted(self._df_fund_old_nisa_accum)
+
+
+    def _get_fund_all(self):
         # 口座管理ページ
         self.driver.find_element(By.XPATH, '//*[@id="link02M"]/ul/li[3]/a/img').click()
         # 投信タブ
@@ -71,17 +91,3 @@ class SBI(BaseSecurities):
                 self._df_fund_nisa_accum = d
             elif d.columns[0] == '投資信託（金額/旧つみたてNISA預り）':
                 self._df_fund_old_nisa_accum = d
-
-        return _get_formatted(self._df_fund_specific)
-
-
-    def get_fund_nisa_accum(self) -> List[Value]:
-        raise NotImplementedError()
-
-
-    def get_fund_old_nisa_accum(self) -> List[Value]:
-        raise NotImplementedError()
-
-
-    def concat(self):
-        raise NotImplementedError()
