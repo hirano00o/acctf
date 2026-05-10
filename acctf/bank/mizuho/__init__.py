77a35174a933d0e81bf0bf5f0971a292004a98fc
hirano00o <uday6827+github@gmail.com>
2026-05-10 10:45:20 +0900

    refactor!: migrate Mizuho Bank to Playwright
    Replace Selenium WebDriver calls with Playwright Page operations:
    - driver.get() -> page.goto()
    - find_element/find_elements with By.* -> selector strings
    - send_keys() -> fill()
    - Select(...).select_by_value() -> page.locator(...).select_option(value=...)
    - Nested elem.find_elements(By.TAG_NAME, "tr") chains -> Locator.locator() chains
    - NoSuchElementException / TimeoutException -> playwright TimeoutError
    
    BREAKING CHANGE: Constructor accepts a Playwright Page instead of a
    Selenium WebDriver.

--- acctf/bank/mizuho/__init__.py
+++ acctf/bank/mizuho/__init__.py
@@ -5,34 +5,29 @@ from io import StringIO
 import pandas as pd
 from bs4 import BeautifulSoup
 from dateutil.relativedelta import relativedelta
-from selenium import webdriver
-from selenium.common import NoSuchElementException, TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
+from playwright.sync_api import Page
+from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
 
 from acctf.bank import Bank, Balance, Transaction
 from acctf.bank.model import str_to_deposit_type, CurrencyType
 
 
 class Mizuho(Bank, ABC):
-    def __init__(self, driver: webdriver = None, timeout: float = 30):
-        super().__init__(driver=driver, timeout=timeout)
-        self.driver.get('https://web.ib.mizuhobank.co.jp/servlet/LOGBNK0000000B.do')
+    def __init__(self, page: Page, timeout: int = 30000):
+        super().__init__(page=page, timeout=timeout)
+        self.page.goto('https://web.ib.mizuhobank.co.jp/servlet/LOGBNK0000000B.do')
 
 
     def login(self, user_id: str, password: str, totp: str | None = None):
-        user_id_elem = self.find_element(By.NAME, 'txbCustNo')
-        user_id_elem.send_keys(user_id)
-        self.driver.find_element(By.NAME, 'N00000-next').click()
+        self.find_element('[name="txbCustNo"]').fill(user_id)
+        self.page.locator('[name="N00000-next"]').click()
 
-        user_pw_elem = self.find_element(By.NAME, 'PASSWD_LoginPwdInput')
-        user_pw_elem.send_keys(password)
-        self.driver.find_element(By.ID, 'btn_login').click()
+        self.find_element('[name="PASSWD_LoginPwdInput"]').fill(password)
+        self.page.locator('#btn_login').click()
 
         try:
-            important_notification = '//*[@id="button-section"]/a/img'
-            elem = self.find_element(By.XPATH, important_notification)
-        except TimeoutException:
+            elem = self.find_element('//*[@id="button-section"]/a/img')
+        except PlaywrightTimeoutError:
             pass
         else:
             elem.click()
@@ -41,31 +36,27 @@ class Mizuho(Bank, ABC):
 
 
     def logout(self):
-        self.driver.find_element(By.XPATH, '//*[@id="side-menu"]/div[1]/a/img').click()
+        self.page.locator('//*[@id="side-menu"]/div[1]/a/img').click()
 
 
     def get_balance(self, account_number: str) -> list[Balance]:
         balance = 'MB_R011N030'
-        elem = self.find_element_to_be_clickable(By.ID, balance)
-        elem.click()
+        self.find_element_to_be_clickable(f'#{balance}').click()
         # When there is the account select box
         try:
-            select = 'n03000-t1'
-            elem = self.find_element(By.CLASS_NAME, select)
-            tr = iter(elem.find_elements(By.TAG_NAME, "tr"))
-            # skip header
-            next(tr)
-            for num, t in enumerate(tr):
-                if t.find_elements(By.TAG_NAME, "span")[2].text == account_number:
-                    t.find_element(By.NAME, f"chkAccChkBx_{str(num).zfill(3)}").click()
+            table_loc = self.find_element('.n03000-t1')
+            rows = table_loc.locator('tr').all()
+            # skip header (rows[0]) and iterate the rest
+            for num, t in enumerate(rows[1:]):
+                spans = t.locator('span').all()
+                if len(spans) > 2 and spans[2].inner_text() == account_number:
+                    t.locator(f'[name="chkAccChkBx_{str(num).zfill(3)}"]').click()
                     break
-            self.driver.find_element(By.XPATH, '//*[@id="main"]/section/input').click()
-        except NoSuchElementException:
-            pass
-        except TimeoutException:
+            self.page.locator('//*[@id="main"]/section/input').click()
+        except PlaywrightTimeoutError:
             pass
 
-        html = self.driver.page_source.encode('utf-8')
+        html = self.page.content().encode('utf-8')
         soup = BeautifulSoup(html, 'html.parser')
         table = soup.find_all("table")
         if table is None or len(table) == 0:
@@ -97,43 +88,35 @@ class Mizuho(Bank, ABC):
         :param currency: currency of transaction history. But this parameter currently doesn't affect.
         """
         transaction = 'MB_R011N040'
-        elem = self.find_element_to_be_clickable(By.ID, transaction)
-        elem.click()
+        self.find_element_to_be_clickable(f'#{transaction}').click()
         # When there is the account select box
         try:
-            select_account = 'lstAccSel'
-            elem = self.find_element(By.NAME, select_account)
-            select = Select(elem)
-            for o in select.options:
-                if o.text.endswith(account_number):
-                    select.select_by_value(o.get_attribute("value"))
+            select_loc = self.find_element('[name="lstAccSel"]')
+            for o in select_loc.locator('option').all():
+                if o.inner_text().endswith(account_number):
+                    value = o.get_attribute("value")
+                    select_loc.select_option(value=value)
                     break
-        except NoSuchElementException:
-            pass
-        except TimeoutException:
+        except PlaywrightTimeoutError:
             pass
 
         if start is not None or end is not None:
             max_date = date.today()
             min_date = date(max_date.year, max_date.month, 1) + relativedelta(months=-2)
             if min_date <= start < end <= max_date:
-                period = 'rdoInqMtdSpec'
-                elem = self.find_elements(By.NAME, period)
-                elem[1].click()
-                Select(self.driver.find_element(By.NAME, 'lstDateFrmYear')).select_by_value(str(start.year))
-                Select(self.driver.find_element(By.NAME, 'lstDateFrmMnth')).select_by_value(str(start.month))
-                Select(self.driver.find_element(By.NAME, 'lstDateFrmDay')).select_by_value(str(start.day))
+                self.find_elements('[name="rdoInqMtdSpec"]').nth(1).click()
+                self.page.locator('[name="lstDateFrmYear"]').select_option(value=str(start.year))
+                self.page.locator('[name="lstDateFrmMnth"]').select_option(value=str(start.month))
+                self.page.locator('[name="lstDateFrmDay"]').select_option(value=str(start.day))
 
-                Select(self.driver.find_element(By.NAME, 'lstDateToYear')).select_by_value(str(end.year))
-                Select(self.driver.find_element(By.NAME, 'lstDateToMnth')).select_by_value(str(end.month))
-                Select(self.driver.find_element(By.NAME, 'lstDateToDay')).select_by_value(str(end.day))
+                self.page.locator('[name="lstDateToYear"]').select_option(value=str(end.year))
+                self.page.locator('[name="lstDateToMnth"]').select_option(value=str(end.month))
+                self.page.locator('[name="lstDateToDay"]').select_option(value=str(end.day))
             else:
                 raise AttributeError(f"date can be set between {min_date} and {max_date}")
-        inquiry_transaction = '//*[@id="main"]/section[1]/input'
-        elem = self.find_element(By.XPATH, inquiry_transaction)
-        elem.click()
+        self.find_element('//*[@id="main"]/section[1]/input').click()
 
-        html = self.driver.page_source.encode('utf-8')
+        html = self.page.content().encode('utf-8')
         soup = BeautifulSoup(html, 'html.parser')
         table = soup.find("table", class_="n04110-t2")
         if table is None:
